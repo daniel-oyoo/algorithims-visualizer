ffeb342b5ac1efd1fd62c786e032c944619db57e
daniel-oyoo <markdaniel4182@gmail.com>
2026-07-28 14:24:09 +0300

    feat : first prototype to render matrix on screen

--- front-end/matrix/basin/script.js
+++ front-end/matrix/basin/script.js
@@ -11,9 +11,12 @@ let map = []; //2d array of our own
 
 let allValid = [];
 
-let rows = 5;
+let rows = 9;
 
-let cols = 5;
+let cols = 9;
+
+const PROB = 1;
+
 
 
 
@@ -32,14 +35,6 @@ function loadMatrix() {
             map[i][j] = Math.floor(Math.random() * rows);
         }
     }
-
-    //map = proxyMap;
-
-    //save map to local storage
-    //localStorage.setItem("map", map);
-
-    //destroy proxy---prolly not wise
-    //proxyMap = null;
 }
 
 function getValidNeighbour(row, col) {
@@ -110,9 +105,91 @@ function findBasin() {
     }
 }
 
+
+
+function deleteRandom() {
+    let choice = Math.floor(Math.random() * 2); //add some bad with good boards dynamically
+    if (choice === 1) { //good board just delete any number--cant have duplicates
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < Math.floor(cols * PROB); j++) {
+                //set random to empty
+                map[Math.floor(Math.random() * rows)][Math.floor(Math.random() * rows)] = 0;
+            }
+        }
+    } else { //replace numbers may have duplicates
+
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < Math.floor(cols * PROB); j++) {
+                //set random to empty
+                map[Math.floor(Math.random() * rows)][Math.floor(Math.random() * rows)] = 0;
+                map[Math.floor(Math.random() * rows)][Math.floor(Math.random() * rows)] = Math.floor(Math.random() * 10);
+            }
+        }
+
+    }
+}
+
+
+function renderMatrix() {
+
+    //let temp = seededMap;
+
+    //console.log(temp);
+
+    map = [
+        [8, 1, 2, 3, 7, 4, 5, 6, 9],
+        [9, 4, 3, 6, 2, 5, 1, 7, 8],
+        [5, 7, 6, 8, 9, 1, 2, 4, 3],
+        [1, 5, 4, 2, 3, 7, 8, 9, 6],
+        [3, 6, 8, 4, 5, 9, 7, 1, 2],
+        [7, 2, 9, 1, 6, 8, 4, 3, 5],
+        [2, 3, 1, 7, 8, 6, 9, 5, 4],
+        [4, 8, 5, 9, 1, 3, 6, 2, 7],
+        [6, 9, 7, 5, 4, 2, 3, 8, 1]
+    ];
+
+    //delete rand
+    deleteRandom();
+
+    let container = document.querySelector('.main-content #matrix');
+    //document.getElementById('matrix');
+
+    console.log(container);
+    //clear
+    container.innerHTML = " ";
+    //1.get table
+    let table = document.createElement('table');
+    table.id = "table-matrix ";
+    table.border = "1 ";
+    for (let i = 0; i < rows; i++) {
+        //create rows
+        let r = document.createElement('tr');
+        for (let j = 0; j < cols; j++) {
+            let c = document.createElement('td');
+            //class
+            c.className = "cell ";
+            //id
+            c.id = `${i}-${j}`;
+            //text maybe
+            c.innerText = map[i][j];
+            //append
+            r.appendChild(c);
+        }
+        table.appendChild(r);
+    }
+
+    container.appendChild(table);
+
+    //seededMap = temp;
+    // map = [];
+}
+
+
 //console.log(map);
 
-loadMatrix();
+//loadMatrix();
+
+renderMatrix();
 
 console.log(map);
 
